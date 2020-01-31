35260cc0749ed7cd836eb92a6a6d0312bc122043
Sugathan.anandan <sugathan.anadan@tallysolutions.com>
2020-01-31 15:11:11 +0530

    pipe project init

---
+++ Client/PipeClient.cpp
@@ -0,0 +1,21 @@
+﻿// PipeClient.cpp : Defines the entry point for the console application.
+//
+
+#include <string>
+#include <io.h>
+#include <fcntl.h>
+#include <windows.h>
+#include "CPipeClient.h"
+
+int main(int argc, char** argv[])
+{
+    _setmode(_fileno(stdout), _O_U8TEXT);
+    std::wcout << TEXT("---------------------Pipe Client--------------------") << std::endl;
+    std::wstring sPipeName(PIPENAME);
+    CPipeClient* pClient = new CPipeClient(sPipeName);
+    ::WaitForSingleObject(pClient->GetThreadHandle(), INFINITE);
+    delete pClient;
+    pClient = NULL;
+    return 0;
+}
+
---
+++ Server/PipeServer.cpp
@@ -0,0 +1,21 @@
+﻿// PipeServer.cpp : Defines the entry point for the console application.
+//
+
+#include <io.h>
+#include <fcntl.h>
+#include "PipeConst.h"
+#include "CPipeServer.h"
+
+int main(int argc, char** argv[])
+{
+    _setmode(_fileno(stdout), _O_U16TEXT);
+    std::wcout << TEXT("---------------------Pipe Server--------------------") << std::endl;
+    std::wstring sPipeName(PIPENAME);
+    CPipeServer* pServer = new CPipeServer(sPipeName);
+    ::WaitForSingleObject(pServer->GetThreadHandle(), INFINITE);
+    delete pServer;
+    pServer = NULL;
+
+    return 0;
+}
+
